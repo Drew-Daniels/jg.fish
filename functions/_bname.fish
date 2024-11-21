ad7388d2c07c3289e87360d230154fafd459330c
Drew Daniels <drew.daniels@kipuhealth.com>
2024-11-20 20:47:36 -0600

    echo args in bname

--- functions/_bname.fish
+++ functions/_bname.fish
@@ -1,6 +1,8 @@
 function _bname -d "Generates a Git branch name using a Jira Ticket ID"
     set -l options h/help q/quiet c/clipboard
 
+    echo "$argv"
+
     argparse $options -- $argv
 
     if set --query _flag_help
