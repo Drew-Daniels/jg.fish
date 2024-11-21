300894586b61fbc277dcb262639990abc6410276
Drew Daniels <drew.daniels@kipuhealth.com>
2024-11-20 21:26:33 -0600

    add error handling for unknown subcmds

--- functions/jg.fish
+++ functions/jg.fish
@@ -5,13 +5,7 @@ function jg --argument-names cmd -d "Jira Genie"
         case -v --version
             echo "jg version, $jg_version"
         case "" help -h --help
-            printf "Usage: jg <COMMAND> [OPTIONS]\n\n"
-            printf "Commands:\n"
-            printf "  help    Prints help and exits\n"
-            printf "  link       Copies Jira Issue Link for Current Git Branch\n"
-            printf "  bname   Generates a Git branch name using a Jira Ticket ID\n"
-            printf "  pr      Generates a Slack Message to Link to a Jira Ticket and Pull Request\n"
-            printf "  prd     Generates a Description for a Given PR\n"
+            _jg_help
         case link
             _jg_link $argv[2..-1]
         case bname
@@ -20,5 +14,8 @@ function jg --argument-names cmd -d "Jira Genie"
             _jg_pr $argv[2..-1]
         case prd
             _jg_prd $argv[2..-1]
+        case *
+            echo "Unknown command: $cmd"
+            _jg_help
     end
 end
