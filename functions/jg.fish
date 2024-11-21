e85e4c4cd5779fd478c08fbbd121faca82af22e3
Drew Daniels <drew.daniels@kipuhealth.com>
2024-11-20 20:11:08 -0600

    create better help message

--- functions/jg.fish
+++ functions/jg.fish
@@ -3,5 +3,11 @@ function jg --argument-names cmd -d "Jira Genie"
 
         case "" -h --help
             printf "Usage: jg <COMMAND>\n\n"
+            printf "Commands:\n"
+            printf "  jlink   Copies Jira Issue Link for Current Git Branch\n"
+            printf "  bname   Generates a Git branch name using a Jira Ticket ID\n"
+            printf "  pr      Generates a Slack Message to Link to a Jira Ticket and Pull Request\n"
+            printf "  prd     Generates a Description for a Given PR\n"
+            return 0
     end
 end
