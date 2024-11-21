ff72cffed4e811fa7df85ee50bc1a31e12594077
Drew Daniels <drew.daniels@kipuhealth.com>
2024-11-20 20:35:28 -0600

    update help message, add help subcmd

--- functions/jg.fish
+++ functions/jg.fish
@@ -5,9 +5,10 @@ function jg -d "Jira Genie"
     switch "$argv[1]"
         case -v --version
             echo "jg version, $jg_version"
-        case "" -h --help
-            printf "Usage: jg <COMMAND>\n\n"
+        case "" help -h --help
+            printf "Usage: jg <COMMAND> [OPTIONS]\n\n"
             printf "Commands:\n"
+            printf "  help    Prints help and exits\n"
             printf "  jlink   Copies Jira Issue Link for Current Git Branch\n"
             printf "  bname   Generates a Git branch name using a Jira Ticket ID\n"
             printf "  pr      Generates a Slack Message to Link to a Jira Ticket and Pull Request\n"
