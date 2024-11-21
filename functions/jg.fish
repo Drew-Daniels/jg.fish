6dde6db1d36b74f861e2fd5b9a1ce99c419838c7
Drew Daniels <drew.daniels@kipuhealth.com>
2024-11-20 20:25:23 -0600

    ?

--- functions/jg.fish
+++ functions/jg.fish
@@ -3,12 +3,8 @@ function jg --argument-names cmd -d "Jira Genie"
     set -l jg_version 0.0.1
 
     switch "$cmd"
-
-        echo "argv: $argv"
-
         case -v --version
             echo "jg version, $jg_version"
-
         case "" -h --help
             printf "Usage: jg <COMMAND>\n\n"
             printf "Commands:\n"
@@ -16,8 +12,6 @@ function jg --argument-names cmd -d "Jira Genie"
             printf "  bname   Generates a Git branch name using a Jira Ticket ID\n"
             printf "  pr      Generates a Slack Message to Link to a Jira Ticket and Pull Request\n"
             printf "  prd     Generates a Description for a Given PR\n"
-            return 0
-
         case jlink
             jlink $argv
         case bname
