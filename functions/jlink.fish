46e79c0d103eff791f7024b0831b8d68ff1e6c41
Drew Daniels <drew.daniels@kipuhealth.com>
2024-11-20 19:37:19 -0600

    remove comment

--- functions/jlink.fish
+++ functions/jlink.fish
@@ -20,7 +20,6 @@ function jlink -d "Copies Jira Issue Link for Current Git Branch"
         return 1
     end
 
-    # parse text after first forward slash and before second forward slash and trim whitespace including newlines and put this into a local variable
     set -l jira_issue_id (git branch --show-current | cut -d / -f2- | cut -d / -f1 | tr -d '[:space:]' | tr a-z A-Z)
 
     if set -q _flag_i
