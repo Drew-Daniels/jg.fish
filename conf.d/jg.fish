d3f8eeeddb1800360b09bcbc38127a90df473f74
Drew Daniels <drew.daniels@kipuhealth.com>
2024-11-21 17:48:13 -0600

    fix event names

--- conf.d/jg.fish
+++ conf.d/jg.fish
@@ -1,8 +1,8 @@
-function __jg_install -e gitnow_install
+function __jg_install -e jg_install
     echo (jg -v)" is installed and ready to use!"
     echo "Just run the `jg` command if you want explore the API."
 end
 
-function __jg_uninstall -e gitnow_uninstall
+function __jg_uninstall -e jg_uninstall
     echo "jg was uninstalled successfully."
 end
